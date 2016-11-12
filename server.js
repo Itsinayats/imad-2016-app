b2645a8bb529040190dc4082e57b7207f0630d2d
inayat hussain <inayat.rdec@gmail.com>
2016-11-12 20:41:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -349,7 +349,7 @@ var template= `
 <div class="container">
   <h2>Modal Example</h2>
   <!-- Trigger the modal with a button -->
-  <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+  <button data-target="#myModal">Open Modal</button>
 
   <!-- Modal -->
   <div class="modal fade" id="myModal" role="dialog">
