c8b00ebae3e6bf25dc524ec908a6008e4f64cda5
inayat hussain <inayat.rdec@gmail.com>
2016-11-12 21:30:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -191,9 +191,13 @@ pool.query("select article_tags.tag,articles.title,articles.content,articles.cat
                      
                  }
              }
+    
 });
+});
+ 
 
- });
+ 
+ 
  
 
  
@@ -305,7 +309,7 @@ background:	#E6E6FA;
       <h1>${heading.toUpperCase()}</h1>
        <h4><span class="label label-info">${category}</span></h4>
       <h5><span class="glyphicon glyphicon-time"></span> Post by <b>${author}</b>, ${date.toDateString()}.</h5>
-      <h5><span class="label label-success">${tag}</span></h5><br>
+      <h5><span class="label label-success"><% tag%></span></h5><br>
       <p>${content}</p>
       <hr>
 
