0519572700230252cc62e50a6eb871a3393afb8b
inayat hussain <inayat.rdec@gmail.com>
2016-11-12 20:39:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -45,7 +45,6 @@ app.get('/test-db', function (req, res) {
 
 
 
-
 //BOOTSTRAP FILES INCLUDES
 app.get('/ui/css/bootstrap.min.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui/css', 'bootstrap.min.css'));  //bootstrap  to test
@@ -186,7 +185,7 @@ pool.query("select article_tags.tag,articles.title,articles.content,articles.cat
                 res.status(400).send('ARTICLE NOT FOUND');
              }
                  else{
-                   
+                
                   var  articleData=result.rows[0];
                   res.send(createTemplate(articleData));
                      
@@ -346,6 +345,42 @@ var template= `
     </div>
   </div>
 </div>
+
+<div class="container">
+  <h2>Modal Example</h2>
+  <!-- Trigger the modal with a button -->
+  <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Modal Header</h4>
+        </div>
+        <div class="modal-body">
+          <p>Some text in the modal.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+  
+</div>
+
+
+
+
+
+
+
+
 
 <footer class="container-fluid">
   <p>Footer Text</p>
