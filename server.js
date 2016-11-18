e866d5e5ef2bb809c5bf3ee51d9eed173cda1dad
inayat hussain <inayat.rdec@gmail.com>
2016-11-18 18:31:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -343,7 +343,7 @@ background:	#E6E6FA;
       <h1>${heading.toUpperCase()}</h1>
        <h4><span class="label label-info">${category}</span></h4>
       <h5><span class="glyphicon glyphicon-time"></span> Post by <b>${author}</b>, ${date.toDateString()}.</h5>
-      <h5><span class="label label-success">${tag}</span></h5><br>
+      <h5><span class="label label-success" id="tags">Loading Tags...</span></h5><br>
       <p>${content}</p>
       <hr>
 
