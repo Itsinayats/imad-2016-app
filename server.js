e6a5e86e1e391fae1c7915c6790c61ef314e737e
inayat hussain <inayat.rdec@gmail.com>
2016-11-18 11:45:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -233,7 +233,7 @@ var template= `
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel="stylesheet" href="ui/style.css">
+  <link rel="stylesheet" href="/ui/style.css">
  
   		<style>
 
