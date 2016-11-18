0e9d681bef1558cf39505322c1adb206d9577e14
inayat hussain <inayat.rdec@gmail.com>
2016-11-19 01:58:36 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -8,7 +8,7 @@ catbtn.onclick=function () {
             var categories = document.getElementById('categories');
             if (request.status === 200) {
                 var content =  "<ul class='list-group' style='width:300px'>";
-                var CategoryList = JSON.parse(this.responseText);
+                var CategoryList = JSON.parse(this.responseText);                                      
                 for (var i=0; i< CategoryList.length; i++) {
                     content += `
                      <li class="list-group-item">${CategoryList[i].name}<span class="badge">12</span></li>
@@ -28,10 +28,7 @@ catbtn.onclick=function () {
 };
 
 
-window.onload=function() {
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
+function loadBlogData(articleData,i) {
             var heading = document.getElementById('heading');
             var category = document.getElementById('category');
             var author = document.getElementById('author');
@@ -39,14 +36,24 @@ window.onload=function() {
             var content  = document.getElementById('content');
             var time  = document.getElementById('time');
             
+               heading.innerHTML=`<h1>${articleData[i].heading}</h1>`;
+               category.innerHTML=`${articleData[i].category}`;
+               author.innerHTML=`<span class="glyphicon glyphicon-time"></span> Post By <b>${articleData[i].name}</b>`;   
+               content.innerHTML=`${articleData[i].content}`;
+               time.innerHTML=`${articleData[i].time}`;
+              
+            
+}
+
+
+window.onload=function() {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+             var content  = document.getElementById('content');
             if (request.status === 200) {
                 var articleData = JSON.parse(this.responseText);
-             
-               heading.innerHTML=`<h1>${articleData[0].heading}</h1>`;
-               category.innerHTML=`${articleData[0].category}`;
-               author.innerHTML=`<span class="glyphicon glyphicon-time"></span> Post By <b>${articleData[0].name}</b>`;
-               content.innerHTML=`${articleData[0].content}`;
-               time.innerHTML=`${articleData[0].time}`;
+                loadBlogData(articleData,0);
               
             } else {
              content.innerHTML=`<b>Error Fetching Content</b>`;
