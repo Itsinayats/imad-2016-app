332f0c01a7ccbcfd40cfbeb4219ba8aa7f69b51d
inayat hussain <inayat.rdec@gmail.com>
2016-11-18 19:00:18 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -26,3 +26,45 @@ catbtn.onclick=function () {
     request.send();
  
 };
+
+function loadTags(articles){
+    
+    
+}
+
+
+
+
+window.onload=function() {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            var heading = document.getElementById('heading');
+            var category = document.getElementById('category');
+            var author = document.getElementById('author');
+            var  = document.getElementById('content');
+           
+            if (request.status === 200) {
+                var articleData = JSON.parse(this.responseText);
+                for (var i=0; i< articleData.length; i++) {
+                    content += `<li>
+                    <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
+                    (${articleData[i].date.split('T')[0]})</li>`;
+                }
+                content += "</ul>";
+                articles.innerHTML = content;
+            } else {
+                articles.innerHTML('Oops! Could not load all articles!');
+            }
+        }
+    };
+    
+    request.open('GET', '/blog', true);
+    request.send(null);
+};
+
+
+
+
+
+
