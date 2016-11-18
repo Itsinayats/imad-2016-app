8a839f66a0005e8cdea75de903d0093c6eacfe0f
inayat hussain <inayat.rdec@gmail.com>
2016-11-18 12:13:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -455,7 +455,7 @@ background:	#E6E6FA;
 
 
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="/ui/main.js"></script>
 </body>
 </html>
