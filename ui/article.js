005e354e2b39f3e4db024c80dfacd90ddf24fdee
inayat hussain <inayat.rdec@gmail.com>
2016-11-19 00:17:30 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -27,7 +27,7 @@ catbtn.onclick=function () {
  
 };
 
-
+function mysqlTimeStampToDate(timestamp) { var regex=/^([0-9]{2,4})-([0-1][0-9])-([0-3][0-9]) (?:([0-2][0-9]):([0-5][0-9]):([0-5][0-9]))?$/; var parts=timestamp.replace(regex,"$1 $2 $3 $4 $5 $6").split(' '); return new Date(parts[0],parts[1]-1,parts[2],parts[3],parts[4],parts[5]); }
 
 
 window.onload=function() {
@@ -48,7 +48,7 @@ window.onload=function() {
                category.innerHTML=`${articleData[0].category}`;
                author.innerHTML=`${articleData[0].name}`;
                content.innerHTML=`${articleData[0].content}`;
-               time.innerHTML=`<h1>${articleData[0].time.toDateString() }.</h1>`;
+               time.innerHTML=`<h1>${articleData[0].time }.</h1>`;
               
             } else {
              
