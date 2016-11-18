09eb47aba22e257160fd2ee5141a2fce51ce7c86
inayat hussain <inayat.rdec@gmail.com>
2016-11-19 01:09:43 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -44,7 +44,7 @@ window.onload=function() {
              
                heading.innerHTML=`<h1>${articleData[0].heading}</h1>`;
                category.innerHTML=`${articleData[0].category}`;
-               author.innerHTML=`${articleData[0].name}`;
+               author.innerHTML=`Post By <b>${articleData[0].name}</b>`;
                content.innerHTML=`${articleData[0].content}`;
                time.innerHTML=`${articleData[0].time}`;
               
