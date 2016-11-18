430568445a2cb6405cbec3801806adc06e83262e
inayat hussain <inayat.rdec@gmail.com>
2016-11-18 12:47:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,33 +1,4 @@
 console.log('Loaded!');
-var catbtn=document.getElementById('catbtn');
-catbtn.onclick=function () {/*
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            var categories = document.getElementById('categories');
-            if (request.status === 200) {
-                var content =  "<ul class='list-group' style='width:320px'>";
-                var CategoryList = JSON.parse(this.responseText);
-                for (var i=0; i< CategoryList.length; i++) {
-                    content += `<li>
-                     <li class="list-group-item">${CategoryList[i].name}<span class="badge">12</span></li>
-                   </li>`;
-                }
-                content += "</ul>";
-                categories.innerHTML = content;
-            } else {
-                categoriess.innerHTML('Oops! Could not load all articles!');
-            }
-        }
-    };
-    
-    request.open('GET', "/get-categories", true);
-    request.send();
-    */
-    alert("hello");
-};
-
-
 
 
 
