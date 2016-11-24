05bf314fa69146952afcbcd1935a660cf4eb7041
inayat hussain <inayat.rdec@gmail.com>
2016-11-24 10:43:32 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -13,13 +13,13 @@ catbtn.onclick=function () {
                 var CategoryList = JSON.parse(this.responseText);                                      
                 for (var i=0; i< CategoryList.length; i++) {
                     content += `
-                     <li class="list-group-item"><a href="#" id="click-category${i}">${CategoryList[i].category}</a><span class="badge">${CategoryList[i].count}</span></li>
+                     <li class="list-group-item"><a id="category-list" onclick="getArticles(this.id)" href="#" >${CategoryList[i].category}</a><span class="badge">${CategoryList[i].count}</span></li>
                    `;
                 }
                 content += "</ul>";
                 categories.innerHTML = content;
             } else {
-                categoriess.innerHTML('Oops! Could not load all articles!');
+                categories.innerHTML('Oops! Could not load all articles!');
             }
         }
     };
@@ -30,64 +30,18 @@ catbtn.onclick=function () {
 };
 
 
-//Load All articles On click of category
- var register = document.getElementById('click-category');
-    register.onclick = function () {
-        // Create a request object
-        var request = new XMLHttpRequest();
-        
-        // Capture the response and store it in a variable
-        request.onreadystatechange = function () {
-          if (request.readyState === XMLHttpRequest.DONE) {
-              // Take some action
-              if (request.status === 200) {
-                  alert('User created successfully');
-                  register.value = 'Registered!';
-              } else {
-                  alert('Could not register the user');
-                  register.value = 'Register';
-              }
-          }
-        };
-        
-        // Make the request
-        var username = document.getElementById('username').value;
-        var password = document.getElementById('password').value;
-        console.log(username);
-        console.log(password);
-        request.open('POST', '/create-user', true);
-        request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({username: username, password: password}));  
-        register.value = 'Registering...';
-    
-    };
-    
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-//load initially latest article
-function loadBlogData(articleData,i) {
             var heading = document.getElementById('heading');
             var category = document.getElementById('category');
             var author = document.getElementById('author');
             var tags  = document.getElementById('tags');
             var content  = document.getElementById('content');
             var time  = document.getElementById('time');
-            
-               heading.innerHTML=`<h1>${articleData[i].heading}</h1>`;
+//load initially latest article
+              function loadBlogData(articleData,i) {
+              heading.innerHTML=`<h1>${articleData[i].heading}</h1>`;
                category.innerHTML=`${articleData[i].category}`;
                author.innerHTML=`<span class="glyphicon glyphicon-time"></span> Post By, <b>${articleData[i].name}</b>`;   
                content.innerHTML=`${articleData[i].content}`;
@@ -97,12 +51,52 @@ function loadBlogData(articleData,i) {
                    x+=`<span class="label label-success">${articleData[j].tag}</span>&nbsp;`;
                }
                tags.innerHTML=x;
+            
               
     }
 
 
 
-
+//Load All articles On click of category
+var category = document.getElementById('category-list').innerHTML;                  //e to set
+function getArticles (category) {
+       var request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+               if (request.status === 200) {
+                    var articleData = JSON.parse(this.responseText);
+                    for(var i=0;i<articleData.length;i++){
+                        
+                heading.innerHTML=`<h1>${articleData[i].heading}</h1>`;
+               category.innerHTML=`${articleData[i].category}`;
+               author.innerHTML=`<span class="glyphicon glyphicon-time"></span> Post By, <b>${articleData[i].name}</b>`;   
+               content.innerHTML=`${articleData[i].content}`;
+               time.innerHTML=`${articleData[i].time.split('T')[0]}`;
+           var x="";
+               for(var j=0;j<articleData.length;j++){
+                   x+=`<span class="label label-success">${articleData[j].tag}</span>&nbsp;`;
+               }
+               tags.innerHTML=x;  
+                        
+                        }
+                    }
+              } else {
+                 categories.innerHTML('Oops! Could not load all articles!');
+              }
+          };
+        }
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/get-articles', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({category: category}));  
+    
+}
+    
 
 //perform on windows start.
 window.onload=function() {
