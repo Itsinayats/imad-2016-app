9c248d6540455854eabd20dbac433234d9736d43
inayat hussain <inayat.rdec@gmail.com>
2016-11-24 10:49:47 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -56,49 +56,6 @@ catbtn.onclick=function () {
     }
 
 
-
-//Load All articles On click of category
-var c = document.getElementById('category-list');
-var category = document.getElementById('category-list').innerHTML;                  //e to set
-c.onclick=function getArticles (category) {
-       var request = new XMLHttpRequest();
-        request.onreadystatechange = function () {
-          if (request.readyState === XMLHttpRequest.DONE) {
-               if (request.status === 200) {
-                    var articleData = JSON.parse(this.responseText);
-                    for(var i=0;i<articleData.length;i++){
-                        
-                heading.innerHTML=`<h1>${articleData[i].heading}</h1>`;
-               category.innerHTML=`${articleData[i].category}`;
-               author.innerHTML=`<span class="glyphicon glyphicon-time"></span> Post By, <b>${articleData[i].name}</b>`;   
-               content.innerHTML=`${articleData[i].content}`;
-               time.innerHTML=`${articleData[i].time.split('T')[0]}`;
-           var x="";
-               for(var j=0;j<articleData.length;j++){
-                   x+=`<span class="label label-success">${articleData[j].tag}</span>&nbsp;`;
-               }
-               tags.innerHTML=x;  
-                        
-                        }
-                    }
-              } else {
-                 categories.innerHTML('Oops! Could not load all articles!');
-              }
-          };
-        };
-        
-        // Make the request
-        var username = document.getElementById('username').value;
-        var password = document.getElementById('password').value;
-        console.log(username);
-        console.log(password);
-        request.open('POST', '/get-articles', true);
-        request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({category: category}));  
-    
-}
-    
-
 //perform on windows start.
 window.onload=function() {
     var request = new XMLHttpRequest();
