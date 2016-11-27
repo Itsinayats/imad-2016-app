a31b5dabafd7cc9a2d45b41809f324d3964ee666
inayat hussain <inayat.rdec@gmail.com>
2016-11-27 09:33:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -279,7 +279,7 @@ app.post('/create-user', function (req, res) {
    var password = req.body.password;
    var salt = crypto.randomBytes(128).toString('hex');
    var dbString = hash(password, salt);
-   pool.query('INSERT INTO users(name,email,password) VALUES ($1, $2, $3)', [name,email, dbString], function (err, result) {
+   pool.query('INSERT INTO users(id,name,email,password) VALUES (1,$1, $2, $3)', [name,email, dbString], function (err, result) {
       if (err) {
           res.status(500).send(err.toString());
       } else {
