b27bb94e51fd732dcdf26fc2fabb1f62cc0eaff8
inayat hussain <inayat.rdec@gmail.com>
2016-11-27 10:15:44 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -267,7 +267,7 @@ window.onload=function() {
  
     var register = document.getElementById('register_btn');
     register.onclick = function () {
-      
+       register.value = 'Registering...';
         var request = new XMLHttpRequest();
         
       
@@ -295,6 +295,6 @@ window.onload=function() {
         request.open('POST', '/create-user', true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({name:name,email:email,password:password}));  
-        register.value = 'Registering...';
+       
     
     };
